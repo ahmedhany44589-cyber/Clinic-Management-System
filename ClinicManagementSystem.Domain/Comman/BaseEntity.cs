6157f63d3c1f086f55f1da7c879d70765febe9d8
Ahmed_Hany_029 <ahmedhany44589@gmail.com>
2026-07-11 21:55:51 +0300

    Add project files.

---
+++ ClinicManagementSystem.Domain/Comman/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManagementSystem.Domain.Comman
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
